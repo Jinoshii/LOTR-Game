f38b455a9042fc1fc00b55f0f2932e25231ec39a
Jeremy M <Jinoshi@IT-020537.local>
2022-11-29 12:39:10 -0600

    Making Progress

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-const gandalf = "Gandalf"
-let health = 60
-let dice = [6]
+const hero = {
+	elementId: "hero",
+	name: "wizard",
+	avatar: "images/wizard.png",
+	health: "60",
+	diceRoll: [3, 1, 4],
+	diceCount: 3
+	}
+
+const monster = {
+	elementId: "monster",
+	name: "Orc",
+	avatar: "images/orc.png",
+	health: "10",
+	diceRoll: [2],
+	diceCount: 1
+}
+	
+
+
+function renderCharacter(data){
+const {elementId, name, avatar, health, diceRoll, diceCount} = data
+
+let diceHTML = ""
+
+for (let i = 0; i < diceCount; i++){
+	diceHTML += `<div class="dice">${diceRoll[i]}</div>`
+}
+
+
+
+	document.getElementById(data.elementId).innerHTML =
+ ` 
+	<div class="character-card">
+		<h4 class="name"> ${name} </h4>
+		<img class="avatar" src="${avatar}">
+		<p class="health">health: <b> ${health} </b></p>
+		<div class="dice-container"> ${diceHTML} </div>
+	</div>
+ `
+}
+
+renderCharacter(hero)
+
+renderCharacter(monster)
+
