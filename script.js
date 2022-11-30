2406d766e2449d316551e22807d5e929e0b4c70d
Jeremy M <Jinoshi@IT-020537.local>
2022-11-30 12:24:20 -0600

    created script to manage a random dice roll

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const hero = {
 	elementId: "hero",
-	name: "wizard",
+	name: "Wizard",
 	avatar: "images/wizard.png",
 	health: "60",
 	diceRoll: [3, 1, 4],
 	diceCount: 3
-	}
+}
 
 const monster = {
 	elementId: "monster",
@@ -15,21 +15,16 @@ const monster = {
 	diceRoll: [2],
 	diceCount: 1
 }
+
 	
 
 
-function renderCharacter(data){
+function renderCharacter(data) {
 const {elementId, name, avatar, health, diceRoll, diceCount} = data
 
-let diceHTML = ""
+const diceHTML = getDiceHtml(diceCount)
 
-for (let i = 0; i < diceCount; i++){
-	diceHTML += `<div class="dice">${diceRoll[i]}</div>`
-}
-
-
-
-	document.getElementById(data.elementId).innerHTML =
+	document.getElementById(elementId).innerHTML =
  ` 
 	<div class="character-card">
 		<h4 class="name"> ${name} </h4>
@@ -40,7 +35,59 @@ for (let i = 0; i < diceCount; i++){
  `
 }
 
-renderCharacter(hero)
 
+
+function getDiceRollArray(diceCount) {
+	const newDiceRolls = []
+	for (let i = 0; i < diceCount; i++) {
+		newDiceRolls.push(Math.floor(Math.random() * 6) + 1)
+	}
+	return newDiceRolls
+}
+
+
+function getDiceHtml(diceCount) {
+	return getDiceRollArray(diceCount).map(function(number){
+		return `<div class="dice">${number}</div>`
+	}).join("")
+}
+
+getDiceRollArray(3)
+
+renderCharacter(hero)
 renderCharacter(monster)
+
+
+
+
+
+	
+	
+
+
+
+
+
+// **EXERCISE 5: RANDOMIZING DICE**
+// 
+// 1. Create a function called getDiceHtml. 
+// 2. getDiceHtml should map over the array of dice rolls returned from getDiceRollArray. Use that to generate the html for the dice with random values. Use this HTML: `<div class="dice">DICE VALUE HERE</div>`
+// 3. Think about the parameters and arguments.
+// 4. Down in renderCharacter(), set diceHtml equals to our new getDiceHtml function. Remember to give it the argument it needs. 
+// 5. Delete any code we no longer need.
+// 
+// 
+// ==Hints:==
+// 
+// * Your function should take in diceCount as a parameter.
+// * It should then return the array it gets by calling getDiceRollArray(). Remember to use as an argument.
+// * There's no need to set up a new variable - you can map over the array returned by getDiceRollArray directly to return the html template for each dice.
+// 
+// ---
+// 
+// **EXERCISE 6: REPLACE THE FOR LOOP**
+// 
+// 1. Replace the for-loop in getDiceRollArray with a new Array().
+
+
 
